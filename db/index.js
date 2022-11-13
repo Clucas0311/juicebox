2cad7f5460f9a488ddadde89ccae707168324307
Charles Lucas <60566705+Clucas0311@users.noreply.github.com>
2022-11-13 14:12:27 -0500

    feat(updated users func): created updated user query

--- db/index.js
+++ db/index.js
@@ -28,8 +28,36 @@ const createUser = async ({ username, password, name, location }) => {
   }
 };
 
+const updateUser = async (id, fields = {}) => {
+  // This is taking the field keys and storing them to a value $1 etch
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+
+  console.log("setString", setString);
+  if (setString.length === 0) return;
+
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+    UPDATE users
+    SET ${setString}
+    WHERE id=${id}
+    RETURNING *;
+   `,
+      Object.values(fields)
+    );
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
 module.exports = {
   client,
   getAllUsers,
   createUser,
+  updateUser,
 };
--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,4 @@
-const { client, getAllUsers, createUser } = require("./index");
+const { client, getAllUsers, createUser, updateUser } = require("./index");
 
 const createInitialUsers = async () => {
   try {
@@ -89,6 +89,12 @@ const testDB = async () => {
     const users = await getAllUsers();
     console.log("getAllUsers:", users);
 
+    console.log("Calling updateUser on users[0]");
+    const updateUserResult = await updateUser(users[0].id, {
+      name: "Newname Sogood",
+      location: "Lesterville, KY",
+    });
+    console.log("Result:", updateUserResult);
     console.log("Finished database tests!");
   } catch (error) {
     console.error("Error testing database!", error);
